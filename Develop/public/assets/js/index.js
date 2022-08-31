336e4b434800d6e373060b2d151d37fcf80a1b76
Thomasple13 <Tle1321@gmail.com>
2022-08-31 17:06:58 -0500

    test

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -14,7 +14,7 @@ if (window.location.pathname === '/notes') {
 
 // Show an element
 const show = (elem) => {
-  elem.style.display = 'inline';
+  elem.style.display = 'inlined';
 };
 
 // Hide an element
